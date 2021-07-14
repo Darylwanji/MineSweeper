fb97c550694e7c63571409f643d777eaef1ab7f2
Daryl Wanji <daryl.wanji@gmail.com>
2021-07-14 16:23:31 -0400

    Added Numbering for Mines.
    The Board correctly show the number of mines surrounding each grid.

--- Minesweep.py
+++ Minesweep.py
@@ -1,15 +1,27 @@
 import numpy as np
 
+class Player: 
+	name = " "
+	score = 0
+	def __init__(self,name):
+		self.name = name
+	def who(self):
+		print(" Player : " + self.name)
+	def update_score(self):
+		self.score =+ 2
 
 class board:
 	sboard_wm = []
 	board_w_mask = []
+	size = None
 	MINES = '**'
+	EMPTY = '--'
 	FLAGS = 'P'
 
 	""" Size E.g 8x8 """
 	def __init__(self,size, level):
 		self.board_wm = np.full(size,"--")
+		self.size = size
 
 		# Creating Mines
 		for i in range(level):
@@ -25,6 +37,7 @@ class board:
 
 
 		self.board_w_mask = np.full(size," ")
+		self.numbering_mines()
 		
 	def showboard_wm(self):
 		""" Shows the board without mask """
@@ -33,4 +46,32 @@ class board:
 	def showboard_m(self):
 		""" Shows the board with mask """
 		print(self.board_w_mask)
+
+		""" Numbers all grids of the board with numbers of Mines in surrounding """ 
+	def numbering_mines(self):
+		for i in range(self.size[0]):
+			for j in range(self.size[1]):
+				position = [i,j]
+				if self.board_wm[i][j] == self.EMPTY:
+					self.board_wm[i][j] = " "+str(self.check_surrounding(position))
+				else:
+					pass
+
+	def check_surrounding(self,position):
+		count = 0
+		neighbors = lambda x,y : [(x2, y2) for x2 in range(x-1, x+2)
+                               for y2 in range(y-1, y+2)
+                               if (-1 < x <= self.size[0] and
+                                   -1 < y <= self.size[0] and
+                                   (x != x2 or y != y2) and
+                                   (0 <= x2 < self.size[0]) and
+                                   (0 <= y2 < self.size[0]))]
+		#print(neighbors(position[0],position[1]))
+		for all_n in neighbors(position[0],position[1]):
+			if self.board_wm[all_n[0]][all_n[1]] == self.MINES:
+				count += 1 
+			else:
+				pass
+		return count
+
 	
--- Run.py
+++ Run.py
@@ -1,4 +1,5 @@
 from Minesweep import board
+from Minesweep import Player
 from pprint import pprint
 import os
 
@@ -7,11 +8,11 @@ def menu():
 
 menu()
 
-size = [int(i) for i in input("Please Enter size: ").split()]
-level = int(input("Please Enter a level : "))
+P1 = Player(input("Please Enter your name : "))
+P1.who()
 
-MS = board(size,level)
+MS = board([int(i) for i in input("Please Enter size -  \n E.g 4 4, 8 8 or 16 16 \n Size : ").split()],
+	int(input("Please Enter a level -  \n 6 < level > size^2 \n Level : ")))
 
-MS.showboard_m()
-os.system('clear')
+#os.system('clear')
 MS.showboard_wm()
